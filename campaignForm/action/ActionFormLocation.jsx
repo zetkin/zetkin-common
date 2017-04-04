1e0da346f810e1a5d8b9c1a31733692a9f66fd29
Jens Börjesson <jens@dreamsinteractive.se>
2017-04-04 15:32:39 +0200

    Add title to ActionFormLocation due to potential trunc

--- campaignForm/action/ActionFormLocation.jsx
+++ campaignForm/action/ActionFormLocation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function ActionFormLocation(props) {
     return (
-        <div className="ActionFormLocation">
+        <div title={ props.location } className="ActionFormLocation">
             { props.location }
         </div>
     );
