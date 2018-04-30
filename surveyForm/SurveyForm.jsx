9c6477f32e5d607f481d9eafeaaefcb9212e4e45
Richard Olsson <r@richardolsson.se>
2018-04-30 10:26:29 +0200

    Add privacy approval to SurveyForm
    Closes #17

--- surveyForm/SurveyForm.jsx
+++ surveyForm/SurveyForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import cx from 'classnames';
-import { injectIntl } from 'react-intl';
+import { FormattedMessage as Msg, injectIntl } from 'react-intl';
 
 import PropTypes from '../../utils/PropTypes';
 import SurveyElement from './SurveyElement';
 import SurveySignature from './SurveySignature';
+import FormattedLink from '../misc/FormattedLink';
 
 
 @injectIntl
@@ -27,6 +28,7 @@ export default class SurveyForm extends React.Component {
 
         this.state = {
             valid: true,
+            privacyApproved: false,
         };
     }
 
@@ -52,12 +54,41 @@ export default class SurveyForm extends React.Component {
 
         let signature = null;
         let submitButton = null;
+        let privacyApproval = null;
         if (this.props.submitEnabled) {
             let submitLabel = this.props.intl.formatMessage(
                 { id: 'surveyForm.submitButton' })
 
             submitButton = (
                 <input type="submit" value={ submitLabel }/>
+            );
+
+            const privacyName = 'privacy.approval';
+            const privacyPolicyHref = this.props.intl.formatMessage(
+                { id: 'surveyForm.privacy.policyLink.href' });
+
+            privacyApproval = (
+                <div className="SurveyForm-privacy">
+                    <Msg tagName="h2" id="surveyForm.privacy.h"/>
+                    <div className="SurveyForm-privacyApproval">
+                        <input type="checkbox" name={ privacyName } id={ privacyName }
+                            checked={ this.state.privacyApproved }
+                            onChange={ this.onPrivacyApprovalChange.bind(this) }
+                            />
+                        <label htmlFor={ privacyName }>
+                            <Msg id="surveyForm.privacy.label"/>
+                        </label>
+                    </div>
+                    <div className="SurveyForm-privacyTerms">
+                        <Msg tagName="p" id="surveyForm.privacy.p"
+                            values={{ org: survey.getIn(['organization', 'title']) }}
+                            />
+                        <FormattedLink
+                            href={ privacyPolicyHref } target='_blank'
+                            msgId="surveyForm.privacy.policyLink.label"
+                            />
+                    </div>
+                </div>
             );
 
             signature = (
@@ -70,8 +101,8 @@ export default class SurveyForm extends React.Component {
         }
 
         let classes = cx('SurveyForm', {
-            valid: this.state.valid,
-            invalid: !this.state.valid,
+            valid: this.state.valid && this.state.privacyApproved,
+            invalid: !(this.state.valid && this.state.privacyApproved),
         });
 
         return (
@@ -80,6 +111,7 @@ export default class SurveyForm extends React.Component {
                     onSubmit={ this.onSubmit.bind(this) }>
                     { elements }
                     { signature }
+                    { privacyApproval }
                     { submitButton }
                 </form>
             </div>
@@ -87,9 +119,15 @@ export default class SurveyForm extends React.Component {
     }
 
     onSubmit(ev) {
-        if (!this.state.valid || !this.props.submitEnabled) {
+        if (!this.state.valid || !this.state.privacyApproved || !this.props.submitEnabled) {
             ev.preventDefault();
         }
+    }
+
+    onPrivacyApprovalChange(ev) {
+        this.setState({
+            privacyApproved: ev.target.checked,
+        });
     }
 
     onSignature(sig, valid) {
